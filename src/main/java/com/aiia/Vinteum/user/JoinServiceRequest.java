8f2424a2951404aac743656fe557c479df2b9847
han16935 <han1693516@gmail.com>
2024-08-04 16:49:07 +0900

    Add name field to ServiceRequest DTO

--- src/main/java/com/aiia/Vinteum/user/JoinServiceRequest.java
+++ src/main/java/com/aiia/Vinteum/user/JoinServiceRequest.java
@@ -7,17 +7,20 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class JoinServiceRequest {
+    private String name;
     private String userId;
     private String password;
 
     @Builder
-    private JoinServiceRequest(String userId, String password) {
+    private JoinServiceRequest(String name, String userId, String password) {
+        this.name = name;
         this.userId = userId;
         this.password = password;
     }
 
-    public static JoinServiceRequest of(String userId, String password){
+    public static JoinServiceRequest of(String name, String userId, String password){
         return JoinServiceRequest.builder()
+                .name(name)
                 .userId(userId)
                 .password(password)
                 .build();
